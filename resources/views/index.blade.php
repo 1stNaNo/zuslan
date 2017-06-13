ae53c0ce4e8b1ee2154098c26d5256ec5b5021ac
ninjin <njn046@me.com>
2017-06-13 17:53:22 +0800

    header fixed

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,6 +2,6 @@
 
 @section('content')
 
-CONTENT
+
 
 @endsection
--- resources/views/layouts/main/main.blade.php
+++ resources/views/layouts/main/main.blade.php
@@ -4,19 +4,50 @@
 	<body>
 		<div class="body">
       <!-- HEADER -->
-			<div class="row row-eq-height" style="padding : 10px; margin: 10px;">
-        <div class="col-md-6">
-            <div class="row">
-              <div class="col-md-2">
-                <img src="img/logo-niislel.gif" class="" style="max-width: 100%; height: auto;"/>
+      <header id="header" data-plugin-options='{"stickyEnabled": false, "stickyEnableOnBoxed": true, "stickyEnableOnMobile": true, "stickyStartAt": 57, "stickySetTop": "-57px", "stickyChangeLogo": true}'>
+        <div class="header-body">
+          <div class="header-container container">
+            <div class="header-row">
+              <div class="header-column">
+                <div class="header-logo">
+                  <a href="/">
+                    <img alt="" width="60" height="60" data-sticky-width="82" data-sticky-height="40" data-sticky-top="33" src="img/logo-niislel.gif">
+                  </a>
+                </div>
+                <div class="logoDescripion">
+                  <p class="logoTag">Нийслэлийн Байгаль</p>
+                  <p class="logoTag">орчны газар</p>
+                </div>
               </div>
-              <div class="col-md-4 align-middle" style="height:100% !important">Нийслэлийн Байгаль орчны газар</div>
+              <div class="header-column">
+                <div class="header-row">
+                  <div class="header-search hidden-xs">
+                    <form id="searchForm" action="page-search-results.html" method="get">
+                      <div class="input-group">
+                        <input type="text" class="form-control" name="q" id="q" placeholder="Хайлт..." required>
+                        <span class="input-group-btn">
+                          <button class="btn btn-default" type="submit"><i class="fa fa-search"></i></button>
+                        </span>
+                      </div>
+                    </form>
+                  </div>
+
+                </div>
+                <div class="header-row">
+                  <div class="header-nav">
+                    <ul class="header-social-icons social-icons hidden-xs">
+                      <li class="social-icons-facebook"><a href="http://www.facebook.com/" target="_blank" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+                      <li class="social-icons-twitter"><a href="http://www.twitter.com/" target="_blank" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+                      <li class="social-icons-linkedin"><a href="http://www.linkedin.com/" target="_blank" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
             </div>
+          </div>
         </div>
-        <div class="col-md-6 align-right">
-          Хайлт
-        </div>
-      </div>
+      </header>
+
       <!-- MENU -->
       <div class="row" style="background: #20c456">
         <div class="col-md-12">
